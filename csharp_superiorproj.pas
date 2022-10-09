d0b9e9681d67233969432047e3c784b834826249
Iwaneq <iwanskikuba@wp.pl>
2022-10-09 19:52:04 +0300

    Initial Commit

---
+++ csharp_superiorproj.pas
@@ -0,0 +1,128 @@
+unit csharp_superiorProj;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Edit1: TEdit;
+    procedure Button13Click(Sender: TObject);
+    procedure Button14Click(Sender: TObject);
+    procedure Button15Click(Sender: TObject);
+    procedure Button16Click(Sender: TObject);
+    procedure Button17Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Edit1Change(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    Operand1, Operand2, Answer: String;
+    Operators: Char;
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Edit1Change(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button17Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button16Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button15Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button14Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button13Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button6Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button8Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
